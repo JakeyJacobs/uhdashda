154674be9ba3f77f989f52ae5fd4e59b91ee9dd6
JakeyJacobs <rbj17184@gmail.com>
2017-01-27 14:42:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var col = {
+  col1: 0,
+  col2: 0,
+  col3: 0,
+};
+
+function setup() {
+  createCanvas(500, 500);
+  background(0, 0, 0);
+}
+
+function draw() {
+  for(var i = 0; i <= 1000; i++) {
+    console.log(1);
+  };
+};
